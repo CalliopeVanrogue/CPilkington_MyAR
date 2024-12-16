2e05e91d05da8c50d2f7452354733d8e2df37b71
CalliopeVanrogue <harleyquinn771@gmail.com>
2024-12-16 00:21:10 -0800

    Final complete

---
+++ Assets/Scenes/Scripts/Modifications.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Modifications : MonoBehaviour
+{
+//Directional Light changed color to dark blue and intensity so that the dark color darkens the scene
+//I increased the fog so that it's rather dense and makes navigation in the game a little harder
+// Skybox colors were changed to be far darker giving the scene more of a dark ambiance
+// THe game object for the player is now a flashlight. I used a cube to make the base of the light a cube to simulate the switch and a sphere to emulate the light bulb
+// The player game object also has a light componenent attached to it so that the ground is lit up as the player walks.
+// Finish Zone was modified. One emits purple particles the other red particles.
+// The finish zone lights were changed to have one red and one purple
+// The finish zone light intensity was changed as well so there is a bit of a far off glow to help guide the player towards the finish line in such a dark scene
+// The particle effecxts that were changed were color, intensity, shape and emission as well as rendering
+}
